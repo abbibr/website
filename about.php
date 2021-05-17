96e29aba2d371956f8a3fcb767010e071f9d01f8
abbibr <ibrohim_abbosov@mail.ru>
2021-05-17 21:59:30 +0500

    ozgardi

--- about.php
+++ about.php
@@ -10,19 +10,21 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <section id="aboutus">
+    <!-- <section id="aboutus">
         <div class="container">
             <div class="row">
                 <div class="col-sm-7">
                     <h2 clas="text-center">Abbosov Ibrohim</h2>
-                    <p id="p">&nbsp;&nbsp;&nbsp; Hi everyone. Welcome to my personal website!
-                    My name is Ibrohim Abbosov and I am 16 years old, I am web (backend developer)!
-                    I work PHP, Python languages and their frameworks Laravel, Django and for database I use mysql. 
-                    I study at Muhammad al-Khorazmiy school. This school specialized for programming!</p>
-                    <p id="p2">&nbsp;&nbsp;&nbsp; So, I have many projects and sites. I made my projects with html, css, bootstrap, 
-                    but I mainly use different templates for frontend. Because I am backend developer. 
-                    So, I always write code php (laravel) and python (django), mysql for backend. Thank you all for seing my website.
-                    If you have some quetions, please contact us!</p>
+                    <div class="content">
+                        <p id="p">&nbsp;&nbsp;&nbsp; Hi everyone. Welcome to my personal website!
+                        My name is Ibrohim Abbosov and I am 16 years old, I am web (backend developer)!
+                        I work PHP, Python languages and their frameworks Laravel, Django and for database I use mysql. 
+                        I study at Muhammad al-Khorazmiy school. This school specialized for programming!</p>
+                        <p id="p2">&nbsp;&nbsp;&nbsp; So, I have many projects and sites. I made my projects with html, css, bootstrap, 
+                        but I mainly use different templates for frontend. Because I am backend developer. 
+                        So, I always write code php (laravel) and python (django), mysql for backend. Thank you all for seing my website.
+                        If you have some quetions, please contact us!</p>
+                    </div>
                 </div>
                 <div class="col-sm-5">
                     <div class="img-wrap">
@@ -31,10 +33,30 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
+    <div class="section">
+        <div class="container">
+            <div class="container-section">
+            <div class="title">
+                <h2 clas="text-center">About Me</h2>
+            </div>
+            <div class="content">
+                <p class="p">&nbsp;&nbsp;&nbsp; Hi everyone. Welcome to my personal website!
+                My name is <span style="color:red;">Ibrohim Abbosov</span> and I am 16 years old, I am web (<span style="color:red;">backend developer</span>)!
+                I work PHP, Python languages and their frameworks Laravel, Django and for database I use mysql. 
+                I study at Muhammad al-Khorazmiy school. This school specialized for programming! 
+                This school usually takes pupils from 5th grade.</p>
+                <p class="p">&nbsp;&nbsp;&nbsp; So, I have many projects and sites. I made my projects with html, css, bootstrap, 
+                but I mainly use different templates for frontend. Because I am backend developer. 
+                So, I always write code php (laravel) and python (django), mysql for backend. I like programming.
+                Thank you all for seing my website.</p>
+            </div>
+        </div>
+        </div>
+    </div>
     <footer>
         <div class="footer-content">
-            <h3>Backend Developer</h3>
+            
             <p>These are my YouTube, Telegram and GitHub profiles!</p>
             <ul class="socials">
                 <li><a href="https://github.com/abbibr"><i class="fab fa-github"></i></a></li>
--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WebSite</title>
-    <link rel="stylesheet" href="nav.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="navbar.css">
 </head>
 <body>
     <nav>
@@ -24,6 +24,9 @@
         </ul>
     </nav>
     <section>
+        <div class="loader">
+            <h1>Abbosov Ibrohim</h1>
+        </div>
     </section>
 </body>
 </html>
