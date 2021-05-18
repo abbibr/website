d7df395ef6bafcb26dcd8d7fe3749d5eb3ed9c56
abbibr <ibrohim_abbosov@mail.ru>
2021-05-18 15:25:14 +0500

    ozgardi

--- about.php
+++ about.php
@@ -5,35 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="my.css">
+    <link rel="stylesheet" href="mine.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <!-- <section id="aboutus">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-7">
-                    <h2 clas="text-center">Abbosov Ibrohim</h2>
-                    <div class="content">
-                        <p id="p">&nbsp;&nbsp;&nbsp; Hi everyone. Welcome to my personal website!
-                        My name is Ibrohim Abbosov and I am 16 years old, I am web (backend developer)!
-                        I work PHP, Python languages and their frameworks Laravel, Django and for database I use mysql. 
-                        I study at Muhammad al-Khorazmiy school. This school specialized for programming!</p>
-                        <p id="p2">&nbsp;&nbsp;&nbsp; So, I have many projects and sites. I made my projects with html, css, bootstrap, 
-                        but I mainly use different templates for frontend. Because I am backend developer. 
-                        So, I always write code php (laravel) and python (django), mysql for backend. Thank you all for seing my website.
-                        If you have some quetions, please contact us!</p>
-                    </div>
-                </div>
-                <div class="col-sm-5">
-                    <div class="img-wrap">
-                        <img src="my.jpg" alt="rasm">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
     <div class="section">
         <div class="container">
             <div class="container-section">
@@ -45,27 +21,23 @@
                 My name is <span style="color:red;">Ibrohim Abbosov</span> and I am 16 years old, I am web (<span style="color:red;">backend developer</span>)!
                 I work PHP, Python languages and their frameworks Laravel, Django and for database I use mysql. 
                 I study at Muhammad al-Khorazmiy school. This school specialized for programming! 
-                This school usually takes pupils from 5th grade. So, I have many projects and sites. I made my projects with html, css, bootstrap, 
-                but I mainly use different templates for frontend. Because I am backend developer. 
+                This school usually takes pupils from 5th grade. So, I have many projects and sites. I made my projects with different templates for frontend. Because I am backend developer. 
                 So, I always write code php (laravel) and python (django), mysql for backend. I like programming.
-                Thank you all for seing my website.</p>
+                Thank you all for seing my website. If you have some questions contact me!</p>
+                <div class="button">
+                    <a href="#">Read More</a>
+                </div>
             </div>
+            <div class="socials">
+                <a href="https://github.com/abbibr"><i class="fab fa-github"></i></a>
+                <a href="https://t.me/backend_dasturlash"><i class="fab fa-telegram"></i></a></li>
+                <a href="https://www.youtube.com/channel/UCXAt5z_P8vUZnAwD4gAs_3Q"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+        <div class="image-section">
+            <img src="mine.jpg" alt="rasm">
         </div>
         </div>
     </div>
-    <footer>
-        <div class="footer-content">
-            
-            <p>These are my YouTube, Telegram and GitHub profiles!</p>
-            <ul class="socials">
-                <li><a href="https://github.com/abbibr"><i class="fab fa-github"></i></a></li>
-                <li><a href="https://t.me/backend_dasturlash"><i class="fab fa-telegram"></i></a></li>
-                <li><a href="https://www.youtube.com/channel/UCXAt5z_P8vUZnAwD4gAs_3Q"><i class="fab fa-youtube"></i></a></li>
-            </ul>
-        </div>
-        <!-- <div class="footer-bottom">
-            <p>This WebSite created by Ibrohim Abbosov</h1>
-        </div> -->
-    </footer>
 </body>
 </html>
