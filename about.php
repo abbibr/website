3ad0b149aef0bffd9e8edfb96a0509a424e851af
abbibr <ibrohim_abbosov@mail.ru>
2021-07-30 01:32:13 +0500

    website yangilandi

--- about.php
+++ about.php
@@ -19,11 +19,11 @@
             <div class="content">
                 <p class="p">&nbsp;&nbsp;&nbsp; Hi everyone. Welcome to my personal website!
                 My name is <span style="color:red;">Ibrohim Abbosov</span> and I am 16 years old, I am web (<span style="color:red;">backend developer</span>)!
-                I work PHP, Python languages and their frameworks Laravel, Django and for database I use mysql. 
+               	I work PHP language and his frameworks Laravel and for database I use mysql and I also use WordPress platform for making websites. 
                 I study at Muhammad al-Khorazmiy school. This school specialized for programming! 
                 This school usually takes pupils from 5th grade. So, I have many projects and sites. I made my projects with different templates for frontend. Because I am backend developer. 
-                So, I always write code php (laravel) and python (django), mysql for backend. I like programming.
-                Thank you all for seing my website. If you have some questions contact me!</p>
+                So, I always write code php (laravel), mysql for backend. I like programming.
+                Thank you all for seing my website. If you have some questions contact me! Please subscribe my GitHub, Telegram,YouTube and Instagram channels.</p>
                 <div class="button">
                     <a href="#">Read More</a>
                 </div>
@@ -31,13 +31,14 @@
             <div class="socials">
                 <a href="https://github.com/abbibr"><i class="fab fa-github"></i></a>
                 <a href="https://t.me/backend_dasturlash"><i class="fab fa-telegram"></i></a></li>
-                <a href="https://www.youtube.com/channel/UCXAt5z_P8vUZnAwD4gAs_3Q"><i class="fab fa-youtube"></i></a>
+                <a href="https://www.youtube.com/backenddasturlash"><i class="fab fa-youtube"></i></a>
+                <a href="https://instagram.com/backend_dasturlash"><i class="fab fa-instagram"></i></a>
             </div>
         </div>
         <div class="image-section">
-            <img src="mine.jpg" alt="rasm">
+            <img src="mine.jpg" alt="rasm" style="border-radius: 12%;">
         </div>
-        </div>
+        </div> 
     </div>
 </body>
 </html>
